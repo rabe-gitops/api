68316e21f33818207f00c49871e93eef60a3e75c
Luca Lombardo <lucalombardo@Replys-Air.lan>
2020-06-29 22:52:47 +0200

    add cors

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import connexion
+from flask_cors import CORS
 
 from swagger_server import encoder
 
@@ -8,6 +9,7 @@ from swagger_server import encoder
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
+    CORS(app.app)
     app.add_api('swagger.yaml', arguments={'title': 'Swagger Rabe GitOps'})
     app.run(port=8080)
 
